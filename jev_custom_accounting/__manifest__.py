8706d5bee607971749519203841bae27f0ccecdd
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-01-18 14:53:17 +0100

    [ADD]jev custom accounting

---
+++ jev_custom_accounting/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'JEV accounting custom',
+    'version': '16.0.1.0.0',
+    'description': 'customization of accounting app for JEV',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'AGPL-3',
+    'category': '',
+    'depends': [
+        'account','account_usability_akretion'
+    ],
+    'data': [
+        'views/account_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
